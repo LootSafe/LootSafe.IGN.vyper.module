8e23b2785d68defdc3f8a8d25b3e39e45ee858d4
Matt Wisniewski <contact.mattdylan@gmail.com>
2018-02-05 15:45:17 -0700

    Create IGN.v.py

---
+++ IGN.v.py
@@ -0,0 +1,42 @@
+owner: address
+
+# Represents the IGN associated to an address
+names: bytes32[address]
+# Represents the address associated to an IGN
+addresses: address[bytes32]
+
+@public
+def __init__ ():
+    self.owner = msg.sender
+    
+@public
+def register (ign: bytes32):
+    # If address is not already registered to an IGN register it
+    assert not self.names[msg.sender]
+    self.names[msg.sender] = ign
+    self.addresses[ign] = msg.sender
+    
+@public
+def getAddress (ign: bytes32) -> address:
+    # Return the address of an IGN
+    return self.addresses[ign]
+
+@public
+def changeAddress (newAddress: address):
+    # Expect sender to have an IGN registered
+    assert self.names[msg.sender]
+    
+    # Expect new address not to be registered already
+    assert not self.names[newAddress]
+    
+    # Get current IGN
+    ign = self.names[msg.sender]
+    
+    # Change address associated to IGN
+    self.addresses[ign] = newAddress
+    
+    # Change name associated to address
+    self.names[newAddress] = ign
+    
+    # Remove old address registration
+    del self.names[msg.sender]
