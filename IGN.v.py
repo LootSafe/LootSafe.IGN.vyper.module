704250a2318c65a0fc0bb967c5a3b73a19c9ec70
Matt Wisniewski <contact.mattdylan@gmail.com>
2018-02-06 16:50:38 -0700

    Update IGN.v.py

--- IGN.v.py
+++ IGN.v.py
@@ -1,5 +1,5 @@
-Register: __log__({ign: bytes32, addr: address})
-Update: __log__({ign: bytes32, oldAddr: address, newAddr: address})
+Register: __log__({ign: bytes <= 32, addr: address})
+Update: __log__({ign: bytes <= 32, oldAddr: address, newAddr: address})
 
 owner: address
 
@@ -8,10 +8,9 @@ owner: address
 # -------------------------------------------
 
 # Represents the IGN associated to an address
-names: public(bytes32[address])
+names: public((bytes <= 32)[address])
 # Represents the address associated to an IGN
-addresses: public(address[bytes32])
-
+addresses: public(address[bytes <= 32])
 
 # -------------------------------------------
 # -------------- Internal -------------------
@@ -22,19 +21,18 @@ def __init__ ():
     self.owner = msg.sender
 
 @public
-def _tag (addr: address, ign: bytes <= 32) -> bytes <= 32:
+def _tag (addr: address, ign: bytes <= 25) -> bytes <= 32:
     # Creates a tagged IGN for example Matt#ef25d
-    name = slice(ign, start=0, len=25)
     bytesAddr = concat('', sha3(as_bytes32(addr)))
     addrHash = slice(bytesAddr, start=0, len=5)
-    return concat(name, "#", addrHash)
-    
+    return concat(ign, "#", addrHash)
+
 # -------------------------------------------
 # -------------- Getters --------------------
 # -------------------------------------------
 
 @public
-def getAddress (ign: bytes32) -> address:
+def getAddress (ign: bytes <= 25) -> address:
     # Return the address of an IGN
     return self.addresses[ign]
 
@@ -44,23 +42,26 @@ def getAddress (ign: bytes32) -> address:
 # -------------------------------------------
 
 @public
-def register (ign: bytes32):
+def register (ign: bytes <= 25):
     # If address is not already registered to an IGN register it
-    assert self.names[msg.sender] == as_bytes32(0)
-    # Expect username to be no more than 25 bytes
-    assert len(concat('', ign)) <= 25
+    assert len(self.names[msg.sender]) == 0
 
-    self.names[msg.sender] = ign
-    self.addresses[ign] = msg.sender
-    log.Register(ign, msg.sender)
+    taggedName = self._tag(msg.sender, ign)
+    
+    # Ensure this name tag combination is not already registered
+    assert as_bytes32(self.addresses[taggedName]) == as_bytes32(0)
+    
+    self.names[msg.sender] = taggedName
+    self.addresses[taggedName] = msg.sender
+    log.Register(taggedName, msg.sender)
 
 @public
 def changeAddress (newAddress: address):
     # Expect sender to have an IGN registered
-    assert self.names[msg.sender] != as_bytes32(0)
+    assert len(self.names[msg.sender]) > 0
     
     # Expect new address not to be registered already
-    assert self.names[newAddress] == as_bytes32(0)
+    assert len(self.names[msg.sender]) == 0
     
     # Get current IGN
     ign = self.names[msg.sender]
