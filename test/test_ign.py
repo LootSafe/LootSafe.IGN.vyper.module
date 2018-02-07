cd71a390b935b485514dcfea3ddaa8b2edbb4498
Matt Wisniewski <contact.mattdylan@gmail.com>
2018-02-06 18:19:25 -0700

    Update test_ign.py

--- test/test_ign.py
+++ test/test_ign.py
@@ -2,3 +2,11 @@ import pytest
 
 from ethereum.abi import ValueOutOfBounds
 from ethereum.tools import tester
+
+@pytest.fixture
+def ign_tester(t):
+    from vyper import compiler
+    t.languages['vyper'] = compiler.Compiler()
+    contract_code = open('IGN.v.py').read()
+    t.c = t.s.contract(contract_code, language='vyper', args=[t.accounts[0]])
+    return t
