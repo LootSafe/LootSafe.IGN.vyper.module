2f8b66db5f0bdb2025507e5e109fb8cedba41143
Matt Wisniewski <contact.mattdylan@gmail.com>
2018-02-06 18:22:46 -0700

    Update test_ign.py

--- test/test_ign.py
+++ test/test_ign.py
@@ -10,3 +10,7 @@ def ign_tester(t):
     contract_code = open('IGN.v.py').read()
     t.c = t.s.contract(contract_code, language='vyper', args=[t.accounts[0]])
     return t
+
+def test_register(ign_tester):
+    # Check that you can register an IGN
+    assert ign_tester.c.register("Matt")
